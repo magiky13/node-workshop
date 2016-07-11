8c17814f37f4290bfc0bdd7a0588e3a3b5c0b9b1
sisi <simdorvil@hotmail.com>
2016-07-11 18:04:08 +0000

    e2

--- hello-world.js
+++ hello-world.js
@@ -1,5 +1,17 @@
+//e1
 console.log('hello world');
 setTimeout(function(){
     console.log('hello world again');
 }, 10000);
+//e2
 
+var sayHiEveryTenSeconds = function(){
+    setTimeout(function(){
+        console.log('hello world again');
+    }, 10000);
+    sayHiEveryTenSeconds();
+};
+
+console.log(sayHiEveryTenSeconds());
+
+//e3
